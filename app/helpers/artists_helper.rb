d8d62ecd2bd3d8285e9dc5b5847e5da3a6860019
lieby weintraub <liebahinda@gmail.com>
2019-07-25 17:18:25 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,6 +3,8 @@ module ArtistsHelper
     if song.artist 
       link_to song.artist_name, artist_path(song.artist)
     else 
+       link_to "Add Artist", edit_song_path(song)
+     end 
     
     
   end 
